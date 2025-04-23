4371fde31672062b54d88e206c80d1fc707ec0bb
nileshsonawne <nileshsonawne871@gmail.com>
2025-04-23 12:48:17 +0530

    Update db.js

--- db/db.js
+++ db/db.js
@@ -2,10 +2,10 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
+    await mongoose.connect(process.env.MONGO_URI); // from the mongoose database string 
     console.log("MongoDB connected");
   } catch (error) {
-    console.error("DB connection failed", error);
+    console.error("DB connection failed", error);  // error message
     process.exit(1);
   }
 };
