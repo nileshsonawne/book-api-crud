1a874e6cde0db874d31addc8b34029b8190139ee
nileshsonawne <nileshsonawne871@gmail.com>
2025-04-23 12:42:42 +0530

    Update bookRoutes.js

--- routes/bookRoutes.js
+++ routes/bookRoutes.js
@@ -2,10 +2,19 @@ const express = require("express");
 const router = express.Router();
 const bookController = require("../controllers/bookController");
 
+// create
 router.post("/books", bookController.createBook);
+
+// get all
 router.get("/books", bookController.getAllBooks);
+
+// get single
 router.get("/books/:id", bookController.getBookById);
+
+// update
 router.put("/books/:id", bookController.updateBook);
+
+// delete
 router.delete("/books/:id", bookController.deleteBook);
 
 module.exports = router;
